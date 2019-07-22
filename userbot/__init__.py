1a19e528cd0b071da8395142792ee390c72adbf0
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2019-07-22 13:37:56 +0200

    Add Google Drive Mirror command (#159)
    * Add Google Drive Mirror command
    
    Downloads a file from telegram or link to the server then uploads to your GDrive.
    
    * Separate .download methods into download_from_url, download_from_tg
    * Add gdrive_upload that handle Authentication and Upload process using PyDrive
    * And gdrive function that deals with telegram stuff
    
    It works for files from telegram or a URL
    
    Usage:
    .mirror [in reply to TG file] or .mirror <link> | <filename>
    
    * android: magisk: correct typo in URL
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -85,6 +85,10 @@ SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
 SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
 DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
+GDRIVE_FOLDER = os.environ.get(
+    "GDRIVE_FOLDER", None
+)
+
 # pylint: disable=invalid-name
 bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
 
